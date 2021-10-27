830181c7c9a1b0479f1b4985ee91f737b1db8396
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-10-27 12:37:10 +0530

    Create Whitespace.java

---
+++ Hello-World/Example/Whitespace.java
@@ -0,0 +1,10 @@
+
+System.out.println("Example of a statement");
+ 
+System.out.println("Another statement");
+ 
+// Output:
+// Example of a statement
+// Another statement
+
+// Whitespace, including spaces and newlines, between statements is ignored.
